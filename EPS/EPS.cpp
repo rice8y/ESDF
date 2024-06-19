66cb9123b5de8b98250b00d93e2da2603b70abfc
rice8y <146926884+rice8y@users.noreply.github.com>
2024-06-20 05:39:04 +0900

    Update EPS.cpp

--- EPS/EPS.cpp
+++ EPS/EPS.cpp
@@ -1,17 +1,29 @@
 #include "EPS.h"
 
-void led(int led, bool s) {
+void led(int led, bool s = true)
+{
   int state = s ? HIGH : LOW;
-  switch (led) {
-    case 0: digitalWrite(PIN_LED0, state); break;
-    case 1: digitalWrite(PIN_LED1, state); break;
-    case 2: digitalWrite(PIN_LED2, state); break;
-    case 3: digitalWrite(PIN_LED3, state); break;
-    default: break;
+  switch (led)
+  {
+  case 0:
+    digitalWrite(PIN_LED0, state);
+    break;
+  case 1:
+    digitalWrite(PIN_LED1, state);
+    break;
+  case 2:
+    digitalWrite(PIN_LED2, state);
+    break;
+  case 3:
+    digitalWrite(PIN_LED3, state);
+    break;
+  default:
+    break;
   }
 }
 
-String stateDipsw() {
+String stateDipsw()
+{
   int state[4];
   state[0] = digitalRead(PIN_DIPSW1);
   state[1] = digitalRead(PIN_DIPSW2);
@@ -19,7 +31,8 @@ String stateDipsw() {
   state[3] = digitalRead(PIN_DIPSW4);
 
   String stateString = "";
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++)
+  {
     stateString += (state[i] == HIGH) ? "1" : "0";
   }
   return stateString;
